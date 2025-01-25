e510c6a2d7de5eba9b4f4365f5659b8bfa3f485c
River Mist <rivmist@gmail.com>
2025-01-25 15:57:58 -0800

    Change name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,7 +65,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          LinkedIn <span className="text-[hsl(280,100%,70%)]">Connect</span>
+          Retain <span className="text-[hsl(280,100%,70%)]">AI</span>
         </h1>
         <h2 className="text-lg font-semibold text-white/70 -mt-6">
           {"Upload screenshots of your interactions to find connections"}
